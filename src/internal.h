0dd866ea0b33bf1073a755fd01a647e3ebf321b9
Vasiliy <vasiliy0399@yandex.ru>
2021-08-03 17:57:43 +0300

    Release v1.0
    - теперь считывание пазла идет не из файла с фиксированным именем, а из файла, который указал пользователь
    - реализовал запуск с параметрами: входные файлы, либо опции (запрос справки, версии)
    - оформил файл 'README.md' (добавил текст, картинки, ссылки, инструкции)

--- src/internal.h
+++ src/internal.h
@@ -12,8 +12,41 @@
 
 // Макросы
 #define BUFFER_SIZE 128
+#define VERSION "1.0"
 
 // Прототипы функций
 uint8_t** readArray(char *, uint8_t *, uint8_t *);
+
+#define MANUAL \
+"\n\e[1mNAME\e[0m\n\
+\tsudoku - solve the sudoku puzzle\n\
+\n\
+\e[1mSYNOPSIS\e[0m\n\
+\t\e[1msudoku\e[0m [\e[4mOPTION\e[0m]\n\
+\t\e[1msudoku\e[0m \e[4mSOURSE\e[0m...\n\
+\n\
+\e[1mDESCRIPTION\e[0m\n\
+\tSolve the sudoku puzzle in the SOURSE.\n\
+\n \
+\tMandatory arguments to long options are mandatory for short options too.\n\
+\n\
+\t\e[1m-h, --help\e[0m\n\
+\t\tprint usage\n\
+\n\
+\t\e[1m-v, --version\e[0m\n\
+\t\tprint version\n\
+\n\
+\e[1mEXAMPLE\e[0m\n\
+\tsudoku file1.dan file2.txt\n\
+\n\
+\e[1mAUTHOR\e[0m\n\
+\tWritten by Vasily Voronin.\n\
+\n\
+\e[1mCOPYRIGHT\e[0m\n\
+\tCopyright (C) 2017 Free Software Foundation, Inc. \
+GCC version 7.5.0 or later.\n\
+\tThis is free software: you are free to change and redistribute it.\n\
+\e[1mSEE ALSO\e[0m\n\
+\tFull documentation at : <https://github.com/VasiliyMatlab/Sudoku>\n\n"
 
 #endif
--- src/main.c
+++ src/main.c
@@ -19,31 +19,41 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Error: not enough input arguments\n");
         exit(EIO);
     }
-    for (uint8_t count = 1; count < argc; count++) {
-        printf("\n\n Solving for %s file\n", argv[count]);
-        unsolved = SIZE*SIZE;
-        uint8_t **Puzzle;
-        Puzzle = createPuzzle(argv[count]);
-        uint8_t mem = unsolved;
-        initSudoku(Puzzle);
-        // Пока все ячейки не заполнены
-        while (unsolved > 0) {
-            checkPuzzle();
-            // Если кол-во оставшихся незаполненных ячеек не изменилось
-            if (unsolved == mem) {
-                printf(" Unable to solve the sudoku\n");
-                printf(" The last calculated field:\n");
-                break;
+    if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h")) {
+        printf(MANUAL);
+        return 0;
+    }
+    else if (!strcmp(argv[1], "--version") || !strcmp(argv[1], "-v")) {
+        printf("sudoku version %s\n", VERSION);
+        return 0;
+    }
+    else {
+        for (uint8_t count = 1; count < argc; count++) {
+            printf("\n\n Solving for %s file\n", argv[count]);
+            unsolved = SIZE*SIZE;
+            uint8_t **Puzzle;
+            Puzzle = createPuzzle(argv[count]);
+            uint8_t mem = unsolved;
+            initSudoku(Puzzle);
+            // Пока все ячейки не заполнены
+            while (unsolved > 0) {
+                checkPuzzle();
+                // Если кол-во оставшихся незаполненных ячеек не изменилось
+                if (unsolved == mem) {
+                    printf(" Unable to solve the sudoku\n");
+                    printf(" The last calculated field:\n");
+                    break;
+                }
+                else
+                    mem = unsolved;
             }
-            else
-                mem = unsolved;
+            // Если все ячейки заполнены
+            if (!unsolved)
+                printf(" Success!\n");
+            Puzzle = convertCellstoPuzzle();
+            printPuzzle(Puzzle);
+            clearSudoku();
         }
-        // Если все ячейки заполнены
-        if (!unsolved)
-            printf(" Success!\n");
-        Puzzle = convertCellstoPuzzle();
-        printPuzzle(Puzzle);
-        clearSudoku();
     }
     return 0;
 }
